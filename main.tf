14a81ce45084c91f96d46188ec57ae7950d23b7f
RackPhilO <105866820+RackPhilO@users.noreply.github.com>
2024-01-30 10:17:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
-    container_name       = "tfstate"
+    resource_group_name  = "rg-terraform"
+    storage_account_name = "stotertues0923"
+    container_name       = "backstage-tuesday"
     key                  = "terraform.tfstate"
     use_oidc             = true
   }
